e4a1d4eed77a40edbbd201b5c2118923da509427
MLValadares <mlvss@iscte-iul.pt>
2023-03-02 14:17:06 +0000

    3.1 feito 3.2 acabar 3.3 rever 3.4 fazer

---
+++ 3.4.py
@@ -0,0 +1,19 @@
+def a(str1, str2):
+    return False
+
+
+def b(str1, str2):
+    return False
+
+
+def c(str1, str2):
+    return False
+
+
+def d(str1, str2):
+    return False
+
+
+str1 = "amor"
+str2 = "roma"
+print(a(str1, str2))
