562fda194056a7dd45957a101024083ec7fc6aca
MLValadares <mlvss@iscte-iul.pt>
2023-03-02 15:39:37 +0000

    3.1 feito 3.2 acabar 3.3 rever 3.4 fazer

--- 3.4.py
+++ 3.4.py
@@ -2,15 +2,17 @@ from timeit import default_timer as timer
 
 
 def a(str1, str2):
-    a = [*str1]
-    b = [*str2]
-    for x in a:
-        for y in b:
-            if x == y:
-                print(b[y])
-                break;
-    print(a)
-    print(b)
+    a=[*str1]
+    b=[*str2]
+    x = range(len(str1))
+    for n1 in x:
+        for n2 in x:
+            if a[n1] == b[n2]:
+                b[n2] = None
+    for x in b:
+        if x is not None:
+            return False;
+    return True;
 
 
 def b(str1, str2):
@@ -32,6 +34,7 @@ def d(str1, str2):
     return False
 
 
-str1 = "am or"
-str2 = "ro ma"
+str1 = "amor"
+str2 = "roma"
 print(a(str1,str2))
+print(b(str1,str2))
