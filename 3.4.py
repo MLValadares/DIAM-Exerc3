7153fdc549c8bf279c3a2f74492501dfa034cb63
MLValadares <mlvss@iscte-iul.pt>
2023-03-02 15:11:17 +0000

    3.1 feito 3.2 acabar 3.3 rever 3.4 fazer

--- 3.4.py
+++ 3.4.py
@@ -1,12 +1,30 @@
+from timeit import default_timer as timer
+
+
 def a(str1, str2):
-    return False
+    a = [*str1]
+    b = [*str2]
+    for x in a:
+        for y in b:
+            if x == y:
+                print(b[y])
+                break;
+    print(a)
+    print(b)
 
 
 def b(str1, str2):
+    a = sorted([*str1])
+    b = sorted([*str2])
+    if a == b:
+        return True
     return False
 
 
-def c(str1, str2):
+def c(str1, str2,):
+    a = sorted([*str1])
+    lista = []
+
     return False
 
 
@@ -14,6 +32,6 @@ def d(str1, str2):
     return False
 
 
-str1 = "amor"
-str2 = "roma"
-print(a(str1, str2))
+str1 = "am or"
+str2 = "ro ma"
+print(a(str1,str2))
